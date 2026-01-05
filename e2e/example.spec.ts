0dfdf3e0304d51898526324ee62eb047f97de9cb
Andrew Weinmann <andrew.weinmann@gmail.com>
2026-01-05 02:44:06 +0000

    Add Vitest and Playwright for testing
    Set up vitest and playwright for unit and end-to-end tests,
    respectively, including scripts to run the tests.

---
+++ e2e/example.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // Expect the app to have loaded with correct title
+  await expect(page).toHaveTitle(/factorio-calculators/);
+});
+
+test("app renders", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // Expect the main app element to be visible
+  await expect(page.locator("#root")).toBeVisible();
+});
