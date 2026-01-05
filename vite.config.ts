616dba201e6369bfc56cc51af2afa9899f2d2075
Andrew Weinmann <andrew.weinmann@gmail.com>
2026-01-05 02:45:52 +0000

    Add devcontainer
    Add a dev container config file for consistency in development. Also
    adjust vite config to ensure the server connects properly while inside a
    dev container.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+  },
   test: {
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
